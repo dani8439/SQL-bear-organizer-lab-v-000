9980fce83adb27323ce877aa27ddfb53df87083f
Dani Schuhman <dani8439@aol.com>
2018-07-27 19:50:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "White", "Fluffy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "M", "Green", "Mean", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "F", "Black", "Sweet", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "M", "Blue", "Belligerent", 0);
